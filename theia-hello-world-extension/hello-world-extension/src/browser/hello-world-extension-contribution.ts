6b27cd1dd4cbcb30570382fb320cac4538bdcbd7
Sebastian Dörl <sebastian.doerl@ottogroup.com>
2018-12-07 18:03:17 +0100

    initial commit

---
+++ theia-hello-world-extension/hello-world-extension/src/browser/hello-world-extension-contribution.ts
@@ -0,0 +1,33 @@
+import { injectable, inject } from "inversify";
+import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry, MessageService } from "@theia/core/lib/common";
+import { CommonMenus } from "@theia/core/lib/browser";
+
+export const HelloWorldExtensionCommand = {
+    id: 'HelloWorldExtension.command',
+    label: "Shows a message"
+};
+
+@injectable()
+export class HelloWorldExtensionCommandContribution implements CommandContribution {
+
+    constructor(
+        @inject(MessageService) private readonly messageService: MessageService,
+    ) { }
+
+    registerCommands(registry: CommandRegistry): void {
+        registry.registerCommand(HelloWorldExtensionCommand, {
+            execute: () => this.messageService.info('Hello World!')
+        });
+    }
+}
+
+@injectable()
+export class HelloWorldExtensionMenuContribution implements MenuContribution {
+
+    registerMenus(menus: MenuModelRegistry): void {
+        menus.registerMenuAction(CommonMenus.EDIT_FIND, {
+            commandId: HelloWorldExtensionCommand.id,
+            label: 'Say Hello'
+        });
+    }
+}
